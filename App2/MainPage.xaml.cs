c90bcfbcaefc2354232be6c58fb02a83de10cd66
madeinouweland <lovanden@outlook.com>
2015-11-09 18:51:03 +0100

    initial

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            lijst.Items.Add("a");
+            lijst.Items.Add("a2");
+            lijst.Items.Add("a4");
+            lijst.Items.Add("dsa");
+            lijst.Items.Add("a2sad");
+            lijst.Items.Add("aasdas4");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lijst.SelectedIndex > -1)
+            {
+                lijst.Items.RemoveAt(lijst.SelectedIndex);
+            }
+        }
+    }
+}
---
+++ App2/SelectAfterRemoveBehavior.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App2
+{
+    // Behaviors need a reference to Universal Windows > Extensions > behaviors SDK (12.0)
+
+    [TypeConstraint(typeof(ListViewBase))]
+    public class SelectAfterRemoveBehavior : DependencyObject, IBehavior
+    {
+        private ListViewBase _associatedObject;
+        private int _index;
+
+        public DependencyObject AssociatedObject
+        {
+            get { return _associatedObject; }
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            _associatedObject = (ListViewBase)associatedObject;
+            _associatedObject.SelectionChanged += OnSelectionChanged;
+            _associatedObject.ContainerContentChanging += OnContainerContentChanging;
+        }
+
+        private void OnContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.InRecycleQueue)
+            {
+                if (_index > _associatedObject.Items.Count - 1)
+                {
+                    _index = _associatedObject.Items.Count - 1;
+                }
+                _associatedObject.SelectedIndex = _index;
+            }
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_associatedObject.SelectedIndex > -1)
+            {
+                _index = _associatedObject.SelectedIndex;
+            }
+        }
+
+        public void Detach()
+        {
+            _associatedObject.SelectionChanged -= OnSelectionChanged;
+            _associatedObject.ContainerContentChanging -= OnContainerContentChanging;
+        }
+    }
+}
